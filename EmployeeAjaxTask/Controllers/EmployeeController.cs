a2585adc0e8b68e81e4dfddf232c0316f69816ca
Bersy S B <bersysilvesterbatrish@outlook.com>
2021-07-11 21:51:57 +0530

    Ajax Task

---
+++ EmployeeAjaxTask/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using EmployeeAjaxTask.Models;
+
+namespace EmployeeAjaxTask.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult EmployeeDetails(EmpDetails empDetails)
+        {
+            List<string> email = new List<string>();
+            email.Add("bersy@gmail.com");
+            email.Add("gemsy@gmail.com");
+            email.Add("arun@gmail.com");
+            email.Add("tharun@gmail.com");
+            email.Add("varun@gmail.com");
+
+            for (int i = 0; i < 5; i++)
+            {
+                if(empDetails.Email==email[i])
+                {
+                    return Json(new { msg = "success", data = empDetails }, JsonRequestBehavior.AllowGet);
+                }
+            }
+            return Json(new { msg = "invalid" }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ EmployeeAjaxTask/Models/EmpDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeAjaxTask.Models
+{
+    public class EmpDetails
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public List<string> Skills { get; set; }
+       
+    }
+}
